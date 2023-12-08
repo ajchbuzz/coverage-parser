ac76e63ec14dd5f612d5429fc06d0016f21cf6fb
Ajay Chawda <manya@Ajays-MacBook-Pro.local>
2023-12-08 12:23:24 +0530

    added a cli support

---
+++ coverage-cli.js
@@ -0,0 +1,24 @@
+const parser = require('./lib/parser');
+const fileParsers = require('./lib/file-parsers');
+const fs = require('fs');
+
+const arg = process.argv;
+const filePath = arg[2];
+const fileType = arg[3];
+
+if (!fs.existsSync(filePath)) {
+    console.error('unable to read file '+filePath);
+    return;
+} 
+// check if filetype is available in fileparsers.types 
+
+if(!fileParsers.types.indexOf(fileType)){
+    console.error('invalid file type, supported types are :'+fileParsers.types);
+    return;
+}
+
+parser
+    .parseGlobs(filePath, {
+        type: fileType
+    })
+    .then(results => console.log(results));
