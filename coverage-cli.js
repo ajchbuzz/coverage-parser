0f610109159a7d65289b8de755a658eae3530d60
Ajay Chawda <manya@Ajays-MacBook-Pro.local>
2023-12-08 12:31:12 +0530

    added bin

--- coverage-cli.js
+++ coverage-cli.js
@@ -2,23 +2,29 @@ const parser = require('./lib/parser');
 const fileParsers = require('./lib/file-parsers');
 const fs = require('fs');
 
-const arg = process.argv;
-const filePath = arg[2];
-const fileType = arg[3];
+function start(){
+    const arg = process.argv;
+    const filePath = arg[2];
+    const fileType = arg[3];
 
-if (!fs.existsSync(filePath)) {
-    console.error('unable to read file '+filePath);
-    return;
-} 
-// check if filetype is available in fileparsers.types 
+    if (!fs.existsSync(filePath)) {
+        console.error('unable to read file '+filePath);
+        return;
+    } 
+    // check if filetype is available in fileparsers.types 
 
-if(!fileParsers.types.indexOf(fileType)){
-    console.error('invalid file type, supported types are :'+fileParsers.types);
-    return;
+    if(!fileParsers.types.indexOf(fileType)){
+        console.error('invalid file type, supported types are :'+fileParsers.types);
+        return;
+    }
+
+    parser
+        .parseGlobs(filePath, {
+            type: fileType
+        })
+        .then(results => console.log(results));
 }
 
-parser
-    .parseGlobs(filePath, {
-        type: fileType
-    })
-    .then(results => console.log(results));
+module.exports = {
+    start: start
+}
