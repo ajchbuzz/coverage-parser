fff52b9c8b686710f0cdd2fc20dce951335f573a
Ajay Chawda <manya@Ajays-MacBook-Pro.local>
2023-12-11 23:06:34 +0530

    modified output format

--- coverage-cli.js
+++ coverage-cli.js
@@ -21,7 +21,45 @@ function start(){
         .parseGlobs(filePath, {
             type: fileType
         })
-        .then(results => console.log(JSON.stringify(results)));
+        .then(results => console.log(JSON.stringify(parseOutput(results))));
+}
+
+function parseOutput(results){
+
+    let data = results.map((record) => {
+
+        let file = record.file;
+        let functions = record.functions;
+        let lines = record.lines;
+        let coveredFunctions = '';
+        let uncoveredFunctions = '';
+        let coveredLines = '';
+        let uncoveredLines = '';
+        let coveredLineCount = 0;
+        let uncoveredLineCount = 0;
+        lines.details.map((detail) => {
+            let line = detail.line;
+            let hit = detail.hit;
+            if(hit == 0){
+                uncoveredLines += '^'+line
+                uncoveredLineCount++;
+            }else{
+                coveredLines += '^'+line
+                coveredLineCount++;
+            }
+        });
+        functions.details.map((detail) => {
+            let functionName = detail.name;
+            let hit = detail.hit;
+            if(hit == 0){
+                uncoveredFunctions += '^'+functionName
+            }else{
+                coveredFunctions += '^'+functionName
+            }
+        })
+        return {file:file, uncoveredFunctions:uncoveredFunctions, coveredFunctions:coveredFunctions, coveredLines:coveredLines, uncoveredLines: uncoveredLines, coverage:(( coveredLineCount/(coveredLineCount+uncoveredLineCount))*100).toFixed(2) };
+    })
+    return data;
 }
 
 module.exports = {
